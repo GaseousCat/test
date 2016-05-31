96a40ae8b05e358570ad4ea67c2880aa1ce7dfc5
GaseousCat <anton28.97@gmail.com>
2016-05-31 21:56:34 +0500

    1

---
+++ Безымянный1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <iterator>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+	srand (time(0));
+	int i, a, b, diff, N;
+	float avg; 
+	vector<int> arr;
+	cout << "Enter the size range: ";
+	cin >> N;
+	
+	system("pause");
+	return 0;
+}
